40a67e90c8111494418d14dfaff0a50e90ca743a
Dominik1799 <dom.horvath17@gmail.com>
2020-11-30 18:29:44 +0100

    asd

---
+++ Graph.h
@@ -0,0 +1,175 @@
+#pragma once
+
+#include <vector>
+#include <optional>
+
+// define 1 if you want to implement it
+#define BONUS 0
+
+// Edge can be constructed with syntax
+// Edge e{ 1, 5, 1.89 };
+struct Edge
+{
+    int v;
+    int u;
+    double weight;
+};
+
+class GraphException : public std::exception {
+public:
+    [[nodiscard]] const char *what() const _GLIBCXX_TXN_SAFE_DYN _GLIBCXX_USE_NOEXCEPT override {
+        return "Invalid graph operation!";
+    }
+};
+
+/* Graph represents one abstract graph.
+ *
+ * Vertices are always numbered from 0.
+ *
+ * Between two vertices,  there can only be one edge.   If edges is in
+ * form of  (v, v), so  both ends are  the same, e.g.   AddEdge({1, 1,
+ * 0.5}) throw an exception.
+ *
+ * Also throw excepton if one or both vertices are out of bounds.
+ *
+ * Graph is not directed so (u, v) is the same edge as (v, u).
+ *
+ * Weights are  limited to positive  values, so 0 and  negative values
+ * are forbidden.  If you encounter such weights throw an exception.
+ */
+
+class Graph
+{
+    // Do not modify public interface
+public:
+    // Construct graph with n vertices and no edges
+    explicit Graph(size_t n);
+
+    /* Will construct graph with given  edges, the vertices are from 0
+     * to whatever is the highest number in the vector of edges.
+     */
+    explicit Graph(const std::vector<Edge>& edges);
+
+    /* Add  edge to  graph. If  the edge  already exists,  replace the
+     * weight.
+     */
+    void AddEdge(const Edge& edge);
+
+    /* Same as AddEdge, but can insert  multiple edges. If one edge is
+     * there more  than once,  use later one  (edge with  higher index
+     * overwrites edge with lower index)
+     */
+    void AddEdges(const std::vector<Edge>& edges);
+
+    /* Return  weight between  vertices  u  and v.  If  edge does  not
+     * exists, behaviour is undefined.
+     */
+    double operator()(int u, int v) const;
+
+    /* Return weight between vertices u and v, if edge does not exists,
+     * throw an exception.
+     */
+    double At(int u, int v) const;
+
+    /* Return true  if there  is an  edge between  u and  v, otherwise
+     * false.
+     */
+    bool Connected(int u, int v) const noexcept;
+
+    /* Return shortest path  from u to v (path with  minimal cost). If
+     * there is no path return nullopt.   You can use dijkstra, or any
+     * other algorithm. Path should start with u and ends with v.
+     *
+     * https://en.wikipedia.org/wiki/Dijkstra%27s_algorithm
+     *
+     * 1 BONUS point will be for  speed of this function.
+     * First 5 implementations will be awarded with the bonus point.
+     */
+    std::optional<std::vector<int>> Path(int u, int v) const;
+
+#if BONUS == 1
+    /* Returns  minimum spanning  tree  for this  graph.  You can  use
+     * kruskal's algorithm
+     * https://en.wikipedia.org/wiki/Kruskal%27s_algorithm
+     */
+    Graph SpannigTree() const;
+#endif
+
+private:
+    std::vector<std::vector<double>> graph;
+};
+
+Graph::Graph(size_t n) {
+    graph = std::vector<std::vector<double>>(n,std::vector<double>(n,0));
+    for (const auto& l : graph){
+        for (auto d : l)
+            std::cout << d << " ";
+        std::cout << std::endl;
+    }
+}
+
+Graph::Graph(const std::vector<Edge> &edges) {
+    size_t maxVert = 0;
+    for (auto& e : edges) {
+        if (e.u == e.v || e.weight <= 0)
+            throw GraphException();
+        maxVert = e.u > maxVert ? e.u : maxVert;
+        maxVert = e.v > maxVert ? e.v : maxVert;
+    }
+    graph = std::vector<std::vector<double>>(maxVert+1,std::vector<double>(maxVert+1,0));
+    for (auto& e : edges) {
+        graph[e.u][e.v] = e.weight;
+        graph[e.v][e.u] = e.weight;
+    }
+    for (const auto& l : graph){
+        for (auto d : l)
+            std::cout << d << " ";
+        std::cout << std::endl;
+    }
+
+}
+
+void Graph::AddEdge(const Edge &edge) {
+    if (edge.u == edge.v || edge.weight <= 0)
+        throw GraphException();
+    graph[edge.u][edge.v] = edge.weight;
+    graph[edge.v][edge.u] = edge.weight;
+}
+
+void Graph::AddEdges(const std::vector<Edge> &edges) {
+    for (auto& e : edges){
+        if (e.u == e.v || e.weight <= 0 || e.u >= graph.size() || e.v >= graph.size())
+            throw GraphException();
+        graph[e.u][e.v] = e.weight;
+        graph[e.v][e.u] = e.weight;
+    }
+}
+
+double Graph::At(int u, int v) const {
+    if (u >= graph.size() || v >= graph.size() || graph[u][v] == 0)
+        throw GraphException();
+    return graph[u][v];
+}
+
+double Graph::operator()(int u, int v) const {
+    return graph[u][v];
+}
+
+bool Graph::Connected(int u, int v) const noexcept {
+    if (u >= graph.size() || v >= graph.size() || graph[u][v] == 0)
+        return false;
+    else
+        return true;
+}
+
+std::optional<std::vector<int>> Graph::Path(int u, int v) const {
+    if (u >= graph.size() || v >= graph.size() || graph[u][v] == 0)
+        return std::nullopt;
+    std::vector<int> result;
+    std::vector<bool> vertMap(graph.size(), false); // sptSet
+    std::vector<double> vertDistance(graph.size(),std::numeric_limits<double>::max()); // dist
+    std::vector<int> backTrack(graph.size(),0);
+    while (tr)
+    return result;
+}
+
--- main.cpp
+++ main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
+#include "Graph.h"
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    Edge e{0,1,5};
+    Edge e1{4,8,9};
+    Edge e2{2,1,3};
+    Edge e3{10,5,2};
+    Edge e4{6,9,1};
+    std::vector<Edge> p;
+    p.push_back(e);
+    p.push_back(e1);
+    p.push_back(e2);
+    p.push_back(e3);
+    p.push_back(e4);
+    Graph varname(p);
+    Graph(5);
+    varname.At(5,4);
     return 0;
 }
